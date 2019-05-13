beed05bd8590ad556fff716db78266b77bcfb127
eshmarry <1442214621@qq.com>
2019-05-13 12:04:41 +0800

    connect SSM

--- src/components/gen/Memory.vue
+++ src/components/gen/Memory.vue
@@ -2,25 +2,25 @@
     <div>
         <el-card class="box-card" style="width:50%;margin:50px auto">
             <div slot="header" class="clearfix">
-                <div><span>总容量:500G</span></div>
-                <div><el-progress :percentage="80"></el-progress></div>
+                <div><span>总容量: 500G</span></div>
+                <el-progress :text-inside="true" text-inside="" :stroke-width="18" :percentage="70"></el-progress>
             </div>
 
-           <!-- <div v-for="o in 4" :key="o" class="text item">
-                {{'列表内容 ' + o }}-->
-                <div class="block">
-                    <span class="demonstration">上传文档</span>
-                    <el-pagination
-                            @size-change="handleSizeChange"
-                            @current-change="handleCurrentChange"
-                            :current-page="currentPage4"
-                            :page-sizes="[100, 200, 300, 400]"
-                            :page-size="100"
-                            layout="total, sizes, prev, pager, next, jumper"
-                            :total="400">
-                    </el-pagination>
-                </div>
-           <!-- </div>-->
+            <!-- <div v-for="o in 4" :key="o" class="text item">
+                 {{'列表内容 ' + o }}-->
+            <div class="block">
+                <span class="demonstration">上传文档</span>
+                <el-pagination
+                        @size-change="handleSizeChange"
+                        @current-change="handleCurrentChange"
+                        :current-page="currentPage4"
+                        :page-sizes="[100, 200, 300, 400]"
+                        :page-size="100"
+                        layout="total, sizes, prev, pager, next, jumper"
+                        :total="400">
+                </el-pagination>
+            </div>
+            <!-- </div>-->
         </el-card>
 
     </div>
@@ -57,11 +57,16 @@
         margin-bottom: 18px;
     }
 
+    .clearfix div {
+        margin: 10px;
+    }
+
     .clearfix:before,
     .clearfix:after {
         display: table;
         content: "";
     }
+
     .clearfix:after {
         clear: both
     }
